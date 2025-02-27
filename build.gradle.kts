5b63890a6a6f7a076ab9b772e0ea27ef067d782a
Ads Dawson <104169244+GangGreenTemperTatum@users.noreply.github.com>
2025-02-27 11:32:55 -0500

    chore: remove na deps (#25)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,6 @@ repositories {
 
 dependencies {
     compileOnly("net.portswigger.burp.extensions:montoya-api:2024.12")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
 
